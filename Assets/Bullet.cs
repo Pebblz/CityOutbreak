60a9c3d7f91de2f20eb24ba6bcf07d616f618bd6
Brandon <Blabrake1@gmail.com>
2020-08-28 18:58:17 -0400

    Player Shit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float Timer = 1;
+    void Update()
+    {
+        if (Timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        Timer -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        //col.gameObject.tag != "Enemy" ||
+        //if ( col.gameObject.tag == "Player")
+        //{
+        //    Destroy(this.gameObject);
+        //}
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //for the direction moving
+    Vector3 moveDir;
+    float Speed = 5;
+    float JumpSpeed = 3;
+    bool IsGrounded = false;
+    float distToGround;
+    public GameObject Bullet;
+    float Rof = .5f;
+    //just in case we would need to find out 
+    //if he is grounded or not for later
+    Rigidbody rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && IsGrounded == true || Input.GetKey(KeyCode.UpArrow) && IsGrounded == true
+            || Input.GetKey(KeyCode.Z) && IsGrounded == true)
+        {
+            rb.AddForce(new Vector3(0, JumpSpeed, 0), ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * Speed * Time.deltaTime;
+           // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.15F);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * Speed * Time.deltaTime;
+          //  transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-Vector3.right), 0.15F);
+        }
+        if(Input.GetKey(KeyCode.Space) && Rof <= 0 || Input.GetKey(KeyCode.X) && Rof <= 0)
+        {
+            GameObject bulClone = Instantiate(Bullet,this.gameObject.transform.position, Quaternion.identity);
+            bulClone.GetComponent<Rigidbody>().velocity = transform.right * 5;
+            Rof = .5f;
+        }
+
+        Rof -= Time.deltaTime;
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "floor")
+        {
+            IsGrounded = true;
+        }
+    }
+
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.name == "floor")
+        {
+            IsGrounded = false;
+        }
+    }
+}
