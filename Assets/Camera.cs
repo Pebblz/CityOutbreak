f112dd36391fcf81b73855db8326cc96c2201fef
Brandon <Blabrake1@gmail.com>
2020-08-28 22:37:51 -0400

    ..................

---
+++ Assets/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Vector3 offset;
+    Transform player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position + offset, 1f);
+    }
+}
---
+++ Assets/FallOffMap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallOffMap : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<Player>().RespawnAtCheckPoint();
+        }
+    }
+}
