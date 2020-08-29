285fd373bee707ec2e9695788e818992f794ac8a
jcorm714 <jcorm714@gmail.com>
2020-08-29 02:50:00 -0400

    Lobbin it

---
+++ Assets/LobScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float force = 5f;
+    public float direction = 1f;
+    private bool thrown;
+    public float Timer = 1f;
+    void Start()
+    {
+        thrown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!thrown)
+        {
+            float forceY = force * Mathf.Sqrt(3f);
+            GetComponent<Rigidbody>().AddForce(new Vector3(force * direction, forceY, 0), ForceMode.Impulse);
+            thrown = true;
+        }
+
+        if (Timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        Timer -= Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name != "Bullet(Clone)" && col.gameObject.tag != "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<Player>().Take3Damage();
+        }
+    }
+}
---
+++ Assets/LobberEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobberEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject lobPrefab;
+    private GameObject player;
+    public float ROF = 4f;
+    private float initROF;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        initROF = ROF;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ROF -= Time.deltaTime;
+        if(ROF <= 0f)
+        {
+            shoot();
+            ROF = initROF;
+        }
+    }
+
+    public void shoot()
+    {
+        var bullet = Instantiate(lobPrefab, this.transform);
+        bullet.GetComponent<LobScript>().direction = getDirectionMagnitudeTowardsPlayer();
+
+    }
+
+    public float getDirectionMagnitudeTowardsPlayer()
+    {
+        return Mathf.Sign(player.transform.position.x - this.transform.position.x);
+    }
+}
