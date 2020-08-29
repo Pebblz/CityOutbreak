6ada96f8941cb7e4926e8e3e2cd68e68bfc25d38
jcorm714 <jcorm714@gmail.com>
2020-08-29 08:21:06 -0400

    Audio stuff

---
+++ Assets/Scripts/AudioLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class AudioLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeout = 4f;
+    public AudioClip clip;
+    private bool played = false;
+    private void Awake()
+    {
+        GetComponent<AudioSource>().clip = clip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!played)
+        {
+            GetComponent<AudioSource>().Play();
+            played = true;
+        }
+        if (timeout <= 0f)
+        {
+            Destroy(this.gameObject);
+        }
+        timeout -= Time.deltaTime;
+    }
+}
