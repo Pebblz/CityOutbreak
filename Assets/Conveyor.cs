8256b63d4f290a547f7b116152806032a1c1232b
Jack Carlan <jcarlan7777@gmail.com>
2020-08-29 01:37:00 -0400

    Spikes and belts

---
+++ Assets/Conveyor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public GameObject belt;
+    public float speed = 20;
+    public Transform end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, end.position, speed * Time.deltaTime);
+    }
+}
