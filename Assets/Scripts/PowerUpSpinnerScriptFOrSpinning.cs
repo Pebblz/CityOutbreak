0bdf599eb42dded75d36002fe85136aabc453f71
Brandon <Blabrake1@gmail.com>
2020-08-29 11:00:50 -0400

    rotating powerps

---
+++ Assets/Scripts/PowerUpSpinnerScriptFOrSpinning.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpinnerScriptFOrSpinning : MonoBehaviour
+{
+    private float rotationSpeed = .5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), rotationSpeed);
+    }
+}
