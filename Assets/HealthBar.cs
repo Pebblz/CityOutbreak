07b65aaf9237057e1501b7d1ac08be7ab90414a1
Brandon <Blabrake1@gmail.com>
2020-08-28 23:26:28 -0400

    UI HeathBar Slider

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    private Slider healthBar;
+    GameObject Player;
+    public Image ColorImg;
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        healthBar = GetComponent<Slider>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = Player.GetComponent<Player>().Hp;
+        if(healthBar.value > 14)
+        {
+            ColorImg.color = Color.green;
+        }
+        if(healthBar.value <= 14 && healthBar.value >= 8)
+        {
+            ColorImg.color = Color.yellow;
+        }
+        if(healthBar.value < 8)
+        {
+            ColorImg.color = Color.red;
+        }
+    }
+}
